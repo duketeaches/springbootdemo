ef3308d558ec47c72610b9bec27ea8a9a7ddef5c
Kazi <kaziabidazad@gmail.com>
2019-06-09 00:42:58 +0530

    bug fix

--- src/main/java/duke/learn/spring/SimpleController.java
+++ src/main/java/duke/learn/spring/SimpleController.java
@@ -1,16 +1,16 @@
 package duke.learn.spring;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 
  * @author Kazi
  *
  */
-@RestController
+@Controller
 public class SimpleController {
 
     @Value("${spring.application.name}")
