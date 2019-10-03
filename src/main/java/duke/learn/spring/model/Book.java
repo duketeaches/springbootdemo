83171dc2c85c9d591e6d33985eb8d65ea0bda76b
Kazi Abid Azad <kaziabidazad@gmail.com>
2019-10-03 22:04:59 +0530

    Added table name for jpa mapping with mysql table

--- src/main/java/duke/learn/spring/model/Book.java
+++ src/main/java/duke/learn/spring/model/Book.java
@@ -8,12 +8,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 /**
  * @author Kazi
  *
  */
 @Entity
+@Table(name = "books")
 public class Book {
 
     @Id
